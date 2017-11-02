49dbbf31ccd7282a0cac9138aed195431047e1a7
Gateway Golf & Country Club <mneneman@gatewaygcc.com>
2017-11-02 13:29:57 -0400

    Add files via upload

---
+++ build/interactive-vr-tour.js
@@ -0,0 +1,117 @@
+var vrView;
+
+// Scenes for the VR/Interactive Tour of the Clubhouse and Practice Range
+var scenes = {
+    lobby: {
+        image: 'ggcc-360-lobby.JPG',
+        preview: 'ggcc-360-lobby-preview.jpg',
+        hotspots: {
+            pro_shop_entrance: {
+                pitch: 0,
+                yaw: 120,
+                radius: 0.05,
+                distance: 2
+            }
+        }
+    },
+    pro_shop_entrance: {
+        image: 'ggcc-360-proshop-entrance.JPG',
+        preview: 'ggcc-360-proshop-entrance-preview.jpg',
+        hotspots: {
+            lobby: {
+                pitch: 0,
+                yaw: 90,
+                radius: 0.05,
+                distance: 2
+            },
+            pro_shop: {
+                pitch: 0,
+                yaw: 180,
+                radius: 0.05,
+                distance: 2
+            }
+        }
+    },
+    pro_shop: {
+        image: 'ggcc-360-proshop.JPG',
+        preview: 'ggcc-360-proshop-preview.jpg',
+        hotspots: {
+            pro_shop_entrance: {
+                pitch: 0,
+                yaw: 90,
+                radius: 0.05,
+                distance: 2
+            }
+        }
+    }
+};
+
+function onLoad() {
+    vrView = new VRView.Player('#vrview', {
+      image: 'blank.png',
+      preview: 'blank.png',
+      is_stereo: false,
+      is_autopan_off: true
+    });
+  
+    vrView.on('ready', onVRViewReady);
+    vrView.on('modechange', onModeChange);
+    vrView.on('click', onHotspotClick);
+    vrView.on('error', onVRViewError);
+    vrView.on('getposition', onGetPosition);
+  }
+  
+  function onVRViewReady(e) {
+    console.log('onVRViewReady');
+    loadScene('lobby');
+  }
+  
+  function onModeChange(e) {
+    console.log('onModeChange', e.mode);
+  }
+  
+  function onGetPosition(e) {
+    console.log(e);
+  
+  }
+  
+  function onHotspotClick(e) {
+    vrView.getPosition()
+    console.log('onHotspotClick', e.id);
+    if (e.id) {
+      loadScene(e.id);
+    }
+  }
+  
+  function loadScene(id) {
+    console.log('loadScene', id);
+  
+    // Set the image
+    vrView.setContent({
+      image: scenes[id].image,
+      preview: scenes[id].preview,
+      is_stereo: false,
+      is_autopan_off: true
+    });
+  
+    // Add all the hotspots for the scene
+    var newScene = scenes[id];
+    var sceneHotspots = Object.keys(newScene.hotspots);
+    for (var i = 0; i < sceneHotspots.length; i++) {
+      var hotspotKey = sceneHotspots[i];
+      var hotspot = newScene.hotspots[hotspotKey];
+  
+      vrView.addHotspot(hotspotKey, {
+        pitch: hotspot.pitch,
+        yaw: hotspot.yaw,
+        radius: hotspot.radius,
+        distance: hotspot.distance
+      });
+    }
+  }
+  
+  function onVRViewError(e) {
+    console.log('Error! %s', e.message);
+  }
+  
+  window.addEventListener('load', onLoad);
